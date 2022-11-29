dc311725c6260ddff299fdff49542ea3aac9bcac
nakatani <c0a2116587@edu.teu.ac.jp>
2022-11-29 16:55:44 +0900

    calc　編集前

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,25 +1,74 @@
+
 import tkinter as tk
 import tkinter.messagebox as tkm
+import math
 
-def button_clik(event):
-    btn=event.widget
-    txt=btn[
-    ]
-    tkm.showinfo(txt,f"{}ボタンが押されました")
+def button_click(event):
+
+    btn = event.widget
+    num = btn["text"]
+    if num[0]==0:
+        entry.delete(0,tk.END)
+
+    if num == "=":
+        siki = entry.get() 
+        if siki[0]==0:
+            res=0
+        res = eval(siki) 
+        entry.delete(0, tk.END) 
+        entry.insert(tk.END, res) 
+    
+    elif num == "CE":
+        entry.delete(0,tk.END)
+        res=0
+
+    #elif num == "x²":
+        #entry.delete(0,tk.END)
+        #entry.insert(tk.END,res)
+        #entry.insert(tk.END,"**")
+
+    elif num == "sqrt":
+        
+        entry.delete(0, tk.END) 
+        entry.insert(tk.END, res) 
+
+    else: 
+        entry.insert(tk.END, num)
 
 
 root = tk.Tk()
-root.title("おためしか")
-root.geometry("500x200")
+root.geometry("300x500")
 
-button=tk.Button(root,text=0,font=30)
-button=tk.Button(root,text=1,font=30)
-button=tk.Button(root,text=2,font=30)
-button=tk.Button(root,text=3,font=30)
-button=tk.Button(root,text=4,font=30,width=,height)
+entry = tk.Entry(root, justify="right", width=10, font=("",40))
+entry.grid(row=0, column=0, columnspan=3)
 
-button.bind("<1>",button_clik)
-button.pack()
 
+r, c = 1, 0
+for num in range(9, -1, -1):
+    button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
+    button.grid(row=r, column=c)
+    button.bind("<1>", button_click)
+    c += 1
+    if c%3 == 0:
+        r += 1
+        c = 0
+
+a,b=4,1
+sonotas=["CE","**","%"]
+for sonota in sonotas:
+    button = tk.Button(root, text=f"{sonota}", width=4, height=2, font=("", 30))
+    button.grid(row=b, column=a)
+    button.bind("<1>", button_click)
+    b+=1
+
+operators = ["+","=","-",".","/","×"]
+for ope in operators:
+    button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
+    button.grid(row=r, column=c)
+    button.bind("<1>", button_click)
+    c += 1
+    if c%3 == 0:
+        r += 1
+        c = 0
 
 root.mainloop()
